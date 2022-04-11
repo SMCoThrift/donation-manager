65df22a73a207a59147a353bfc6abd8aa357350c
Michael Wender <michael@michaelwender.com>
2022-04-11 11:00:47 -0400

    First commit

---
+++ donation-manager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Donation Manager
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Online donation manager built for SMCoThrift and PickUpMyDonation.com. This plugin displays the donation form and handles donation submissions.
+ * Author:          Michael Wender
+ * Author URI:      https://mwender.com
+ * Text Domain:     donation-manager
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Donation_Manager
+ */
+
+// Your code starts here.
