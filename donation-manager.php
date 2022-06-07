1e8849e8ec52fab430e069747df731f3469a4d21
Michael Wender <michael@michaelwender.com>
2022-06-07 11:27:15 -0400

    Refactoring, moving functions to their own files

--- donation-manager.php
+++ donation-manager.php
@@ -12,7 +12,11 @@
  * @package         Donation_Manager
  */
 
-// Your code starts here.
+/**
+ * Setup constants.
+ *
+ * @var        string  $css_dir Either `css` or `dist` depending on Site URL.
+ */
 $css_dir = ( stristr( site_url(), '.local' ) || SCRIPT_DEBUG )? 'css' : 'dist' ;
 define( 'DONMAN_CSS_DIR', $css_dir );
 define( 'DONMAN_DEV_ENV', stristr( site_url(), '.local' ) );
@@ -23,44 +27,9 @@ define( 'DONMAN_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
  * Load required files
  */
 require_once( DONMAN_PLUGIN_PATH . 'lib/fns/acf.php' );
+require_once( DONMAN_PLUGIN_PATH . 'lib/fns/debugging.php' );
 
 /**
- * Load Composer or display a notice if not loaded.
+ * Load required libraries and check for required plugins.
  */
-if( file_exists( DONMAN_PLUGIN_PATH . 'vendor/autoload.php' ) ){
-  require_once DONMAN_PLUGIN_PATH . 'vendor/autoload.php';
-} else {
-  add_action( 'admin_notices', function(){
-    $class = 'notice notice-error';
-    $message = __( 'Missing required Composer libraries. Please run `composer install` from the root directory of this plugin.', 'donman' );
-    printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
-  } );
-}
-
-/**
- * Check for ACF
- */
-if( ! class_exists( 'ACF' ) ){
-  add_action( 'admin_notices', function(){
-    $class = 'notice notice-error';
-    $message = __( 'Missing <a href="https://www.advancedcustomfields.com" target="_blank">Advanced Custom Fields</a> plugin. Please install and activate.', 'donman' );
-    printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), $message );
-  } );
-}
-
-/**
- * Enhanced logging.
- *
- * @param      string  $message  The log message
- */
-function uber_log( $message = null ){
-  static $counter = 1;
-
-  $bt = debug_backtrace();
-  $caller = array_shift( $bt );
-
-  if( 1 == $counter )
-    error_log( "\n\n" . str_repeat('-', 25 ) . ' STARTING DEBUG [' . date('h:i:sa', current_time('timestamp') ) . '] ' . str_repeat('-', 25 ) . "\n\n" );
-  error_log( "\n" . $counter . '. ' . basename( $caller['file'] ) . '::' . $caller['line'] . "\n" . $message . "\n---\n" );
-  $counter++;
-}
+require_once DONMAN_PLUGIN_PATH . 'lib/required-checks.php';
---
+++ lib/required-checks.php
@@ -0,0 +1,54 @@
+<?php
+
+// check for plugin using plugin name
+function donman_is_active_plugin( $plugin ){
+  return ( in_array( $plugin, apply_filters( 'active_plugins', get_option('active_plugins') ) ) );
+}
+
+/**
+ * Load Composer or display a notice if not loaded.
+ */
+if( file_exists( DONMAN_PLUGIN_PATH . 'vendor/autoload.php' ) ){
+  require_once DONMAN_PLUGIN_PATH . 'vendor/autoload.php';
+} else {
+  add_action( 'admin_notices', function(){
+    $class = 'notice notice-error';
+    $message = __( 'Missing required Composer libraries. Please run `composer install` from the root directory of this plugin.', 'donation-manager' );
+    printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
+  } );
+}
+
+/**
+ * Load "Persist Admin notice Dismissals" Composer plugin
+ *
+ * See: https://github.com/w3guy/persist-admin-notices-dismissal
+ */
+add_action( 'admin_init', array( 'PAnD', 'init' ) );
+
+/**
+ * Check for ACF
+ */
+if( ! class_exists( 'ACF' ) ){
+  add_action( 'admin_notices', function(){
+    $class = 'notice notice-error';
+    $message = __( 'Missing <a href="https://www.advancedcustomfields.com" target="_blank">Advanced Custom Fields</a> plugin. Please install and activate.', 'donation-manager' );
+    printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), $message );
+  } );
+}
+
+/**
+ * Check for CPT UI
+ */
+if( ! donman_is_active_plugin( 'custom-post-type-ui/custom-post-type-ui.php' ) ){
+  if( ! PAnD::is_admin_notice_active( 'notice-cptui-forever' ) ){
+    add_action( 'admin_notices', function(){
+      $class = 'notice notice-info is-dismissible';
+      $message = __( 'Note: The <a href="https://wordpress.org/plugins/custom-post-type-ui/" target="_blank">CPT UI plugin</a> is not installed. Therefore we\'re using the CPT and Taxonomy definitions defined in the Donation Manager plugin\'s <code>lib/cpt/</code> directory.', 'donation-manager' );
+      printf( '<div class="%1$s" data-dismissible="notice-cptui-forever"><p>%2$s</p></div>', esc_attr( $class ), $message );
+    } );
+  }
+  $files = array_diff( scandir( DONMAN_PLUGIN_PATH . 'lib/cpt' ), [ '.', '..' ] );
+  foreach( $files as $file ){
+    require_once DONMAN_PLUGIN_PATH . 'lib/cpt/' . $file;
+  }
+}
