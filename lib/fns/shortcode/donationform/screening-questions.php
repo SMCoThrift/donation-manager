f34a8c08af638a321ea25bcf4dcd6cdca577c691
Michael Wender <michael@michaelwender.com>
2022-08-12 09:47:17 -0400

    Documenting vars, correctly enqueuing Cloudinary assets

--- lib/fns/shortcode/donationform/screening-questions.php
+++ lib/fns/shortcode/donationform/screening-questions.php
@@ -8,7 +8,7 @@ use function DonationManager\realtors\{get_realtor_ads};
 
 $screening_questions = get_screening_questions( $_SESSION['donor']['org_id'] );
 
-$questions = array();
+$questions = [];
 foreach( $screening_questions as $question ) {
   $question_id = $question['id'];
   $checked_yes = ( isset( $_POST['donor']['answers'][$question_id] ) &&  'Yes' == $_POST['donor']['answers'][$question_id] )? ' checked="checked"' : '';
@@ -23,10 +23,19 @@ foreach( $screening_questions as $question ) {
 }
 
 /**
- * 08/11/2022 (09:18) - TODO: Get Additional Details working...
+ * Setup our Pick Up Settings:
+ *
+ * @type  bool  $provide_additional_details Does the organization allow donors to provide additional details when answering "Yes" to one of the screening questions?
+ * @type  bool  $allow_user_photo_uploads   Does the organization allow donors to upload photos of their donations?
  */
-$provide_additional_details = get_post_meta( $_SESSION['donor']['org_id'], 'provide_additional_details', true );
-$additional_details = ( isset( $_POST['donor']['additional_details'] ) )? esc_textarea( $_POST['donor']['additional_details'] ) : '';
+$pickup_settings = get_field( 'pickup_settings', $_SESSION['donor']['org_id'] );
+
+$provide_additional_details = $pickup_settings['provide_additional_details'];
+$additional_details = null;
+if( $provide_additional_details )
+    $additional_details = ( isset( $_POST['donor']['additional_details'] ) )? esc_textarea( $_POST['donor']['additional_details'] ) : '' ;
+
+$allow_user_photo_uploads = $pickup_settings['allow_user_photo_uploads'];
 
 $hbs_vars = [
     'questions' => $questions,
@@ -41,10 +50,10 @@ $hbs_vars = [
 // jQuery/Cloudinary Photo Upload
 if( $allow_user_photo_uploads )
 {
-    wp_enqueue_script( 'blueimp-jquery-ui-widget', plugin_dir_url( __FILE__ ) . 'lib/components/vendor/blueimp-file-upload/js/vendor/jquery.ui.widget.js', ['jquery'], '2.3.0' );
-    wp_enqueue_script( 'blueimp-iframe-transport', plugin_dir_url( __FILE__ ) . 'lib/components/vendor/blueimp-file-upload/js/jquery.iframe-transport.js', ['jquery'], '2.3.0' );
-    wp_enqueue_script( 'blueimp-file-upload', plugin_dir_url( __FILE__ ) . 'lib/components/vendor/blueimp-file-upload/js/jquery.fileupload.js', ['jquery'], '2.3.0' );
-    wp_enqueue_script( 'cloudinary-file-upload', plugin_dir_url( __FILE__ ) . 'lib/components/vendor/cloudinary-jquery-file-upload/cloudinary-jquery-file-upload.js', ['jquery'], '2.3.0' );
+    wp_enqueue_script( 'blueimp-jquery-ui-widget', DONMAN_PLUGIN_URL . 'lib/components/vendor/blueimp-file-upload/js/vendor/jquery.ui.widget.js', ['jquery'], '2.3.0' );
+    wp_enqueue_script( 'blueimp-iframe-transport', DONMAN_PLUGIN_URL . 'lib/components/vendor/blueimp-file-upload/js/jquery.iframe-transport.js', ['jquery'], '2.3.0' );
+    wp_enqueue_script( 'blueimp-file-upload', DONMAN_PLUGIN_URL . 'lib/components/vendor/blueimp-file-upload/js/jquery.fileupload.js', ['jquery'], '2.3.0' );
+    wp_enqueue_script( 'cloudinary-file-upload', DONMAN_PLUGIN_URL . 'lib/components/vendor/cloudinary-jquery-file-upload/cloudinary-jquery-file-upload.js', ['jquery'], '2.3.0' );
 
     \Cloudinary::config([
         'cloud_name' => CLOUDINARY_CLOUD_NAME,
@@ -59,14 +68,17 @@ if( $allow_user_photo_uploads )
             if ($value) $params[$param] = $value;
         }
         $params = json_encode( $params );
-        $script = str_replace( '{{params}}', $params, file_get_contents( trailingslashit( DONMAN_DIR ) . 'lib/js/cloudinary.js' ) );
+        $script = str_replace( '{{params}}', $params, file_get_contents( DONMAN_PLUGIN_PATH . 'lib/js/cloudinary.js' ) );
         echo '<script type="text/javascript">' . $script . '</script>';
 
-        $dm_styles = str_replace( '{{plugin_uri}}', DONMAN_URL, file_get_contents( trailingslashit( DONMAN_DIR ) . 'lib/css/styles.css' ) );
+        $dm_styles = str_replace( '{{plugin_uri}}', DONMAN_PLUGIN_URL, file_get_contents( DONMAN_PLUGIN_PATH . 'lib/css/styles.css' ) );
         echo '<style type="text/css">' . $dm_styles . '</style>';
     }, 9999 );
 
     // Generate a signed file upload field
+    // 08/12/2022 (09:18) - the file `cloudinary_cors.html` does
+    // not exist anywhere that I've checked (production or dev).
+    // Perhaps this function works by calling a pseudo URL?
     $file_upload_input = cl_image_upload_tag( 'user_photo_id[]', [
         'callback' => site_url() . '/cloudinary_cors.html',
         'html'  => [
