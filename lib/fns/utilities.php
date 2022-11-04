18cd0cfec5170e14909dcd4d494736da6b864f8d
Michael Wender <michael@michaelwender.com>
2022-11-04 14:23:47 -0400

    Adding get_boilerplate()

--- lib/fns/utilities.php
+++ lib/fns/utilities.php
@@ -39,6 +39,32 @@ function get_alert( $atts ){
 }
 
 /**
+ * Gets the boilerplate.
+ *
+ * @param      array  $atts   The atts
+ *
+ * @return     string  The boilerplate.
+ */
+function get_boilerplate( $atts ){
+    extract( shortcode_atts( array(
+        'title' => null,
+    ), $atts ) );
+
+    if( is_null( $title ) )
+        return;
+
+    switch( $title ){
+        case 'about-pmd':
+        case 'aboutpmd':
+            $html = '<h3 style="clear: both; display: block; margin-top: 40px;">About PickUpMyDonation.com</h3>
+Our mission is to connect you with organizations who will pick up your donation. Our donation process is quick and simple. Schedule your donation pick up with our online donation pick up form. Our system sends your donation directly to your chosen pick up provider. They will then contact you to finalize your selected pick up date.';
+        break;
+    }
+
+    return $html;
+}
+
+/**
  * Returns first array value from $_SESSION[‘donor’][‘url_path’]
  *
  * @since 1.?.?
