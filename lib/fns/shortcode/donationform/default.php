c11519a6e11b2e875b7d6e1199e6f5d390b2a3b2
Michael Wender <michael@michaelwender.com>
2023-02-09 11:43:30 -0500

    Using `template` argument value to load donation form template

--- lib/fns/shortcode/donationform/default.php
+++ lib/fns/shortcode/donationform/default.php
@@ -2,7 +2,7 @@
 use function DonationManager\templates\{render_template};
 use function DonationManager\globals\{add_html};
 
-$html = render_template( 'form0.enter-your-zipcode', [ 'nextpage' => $nextpage ] );
+$html = render_template( $args['template'], [ 'nextpage' => $nextpage ] );
 add_html( $html );
 if( current_user_can( 'activate_plugins') && ! isset( $_COOKIE['dmdebug'] ) ){
   add_html( '<div style="text-align: center; font-size: 12px; margin-top: -20px;"><a href="./?dmdebug=true">Start Debug Mode</a></div>' );
