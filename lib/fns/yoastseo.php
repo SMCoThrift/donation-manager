0025def7f4a5b775ecfc2bd3bbbe306ff9e35b05
Michael Wender <michael@michaelwender.com>
2023-03-20 14:25:05 -0400

    Checking if property exists in obj

--- lib/fns/yoastseo.php
+++ lib/fns/yoastseo.php
@@ -12,7 +12,7 @@ namespace DonationManager\yoastseo;
  */
 function add_seo_page_metadesc( $description ){
   global $post;
-  if( ! is_null( $post->post_content ) && has_shortcode( $post->post_content, 'organization-seo-page' ) ){
+  if( property_exists( $post, 'post_content' ) && ! is_null( $post->post_content ) && has_shortcode( $post->post_content, 'organization-seo-page' ) ){
     //return 'This is a test description.';
 
     $regex_pattern = get_shortcode_regex();
