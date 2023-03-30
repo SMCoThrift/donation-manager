b667fa0b5df206b89f28c03c0960aa126dd7fa70
Michael Wender <michael@michaelwender.com>
2023-03-30 13:13:32 -0400

    Adding documentation

--- lib/classes/donation-router.php
+++ lib/classes/donation-router.php
@@ -1,30 +1,42 @@
 <?php
 class DonationRouter {
-    private static $instance = null;
+  private static $instance = null;
 
-    public static function get_instance() {
-        if( null == self::$instance ) {
-            self::$instance = new self;
-        }
+  public static function get_instance() {
+      if( null == self::$instance ) {
+          self::$instance = new self;
+      }
 
-        return self::$instance;
+      return self::$instance;
+  }
+
+  private function __construct() {
+
+  }
+
+  /**
+   * Saving the data we are posting to an API.
+   *
+   * @param      int    $donation_id  The donation ID
+   * @param      array  $args         The arguments we are posting to the API.
+   */
+  public function save_api_post( $donation_id, $args ){
+    update_post_meta( $donation_id, 'api_post', $args );
+  }
+
+  /**
+   * Saves an API response. Serializes response before saving.
+   *
+   * @param      string  $donation_id  The donation identifier
+   * @param      mixed  $response     The response
+   */
+  public function save_api_response( $donation_id, $response ){
+    $message = ( is_wp_error( $response ) )? $response->get_error_message() : serialize( $response );
+    if( ! is_null( $donation_id ) ){
+      update_post_meta( $donation_id, 'api_response', $message );
+    } else {
+      wp_mail( 'webmaster@pickupmydonation.com', 'API Post Error', 'We received the following error when attempting to post Donation #' . $donation_id . ' by API:' . "\n\n" . $message );
     }
-
-    private function __construct() {
-
-    }
-
-    public function save_api_post( $donation_id, $args ){
-        update_post_meta( $donation_id, 'api_post', $args );
-    }
-
-    public function save_api_response( $donation_id, $response ){
-        $message = ( is_wp_error( $response ) )? $response->get_error_message() : serialize( $response );
-        if( ! is_null( $donation_id ) ){
-            update_post_meta( $donation_id, 'api_response', $message );
-        } else {
-            wp_mail( 'webmaster@pickupmydonation.com', 'API Post Error', 'We received the following error when attempting to post Donation #' . $donation_id . ' by API:' . "\n\n" . $message );
-        }
-    }
+  }
 }
 ?>
