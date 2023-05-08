7619f8fda9e34000b7ce81c1ec55884aa90974f6
Michael Wender <michael@michaelwender.com>
2023-05-08 14:06:08 -0400

    Updating Click to Claim link text in compiled file

--- donation-manager.php
+++ donation-manager.php
@@ -7,7 +7,7 @@
  * Author URI:      https://mwender.com
  * Text Domain:     donation-manager
  * Domain Path:     /languages
- * Version:         3.6.5.3
+ * Version:         3.6.5.4
  *
  * @package         DonationManager
  */
