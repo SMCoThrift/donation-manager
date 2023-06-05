6687ed63451e94143c0e2bfa18c7d3f7dd8679da
hashsalacop123 <30858724+hashsalacop123@users.noreply.github.com>
2023-06-05 21:42:56 +0800

    zipcode multiple  checker

--- lib/fns/user-dashboard.php
+++ lib/fns/user-dashboard.php
@@ -51,35 +51,82 @@ function my_publish_organization( $post_id ) {
 add_action( 'acf/save_post', __NAMESPACE__ . '\\my_publish_organization', 20 );
 
 
+// function is_pickup_code_available_callback() {
+//   $code = $_POST['code'];
+//   $term = get_term_by('name', $code, 'pickup_code');
+//   $current_user = wp_get_current_user();
+//   $organization_id = get_user_meta( $current_user->ID, 'organization', true );
+//   $pickup_settings = get_field('pickup_settings', $organization_id);
+//   $priority_pickup = $pickup_settings['priority_pickup'];
+//   $assigned_posts = get_posts(['post_type' => 'trans_dept','tax_query' => [['taxonomy' => 'pickup_code','field' => 'term_id','terms' => $term->term_id]]
+//   ]);
+//   // error_log(print_r($priority_pickup), true);
+//   if (empty($assigned_posts)) {
+//         //check if its assign to any pickup code if not then available
+//     echo 'This pickup code is available.';
+//   } else {
+//         //if not available validate if the priority_pickup is set to true or false 
+//         if($priority_pickup) {
+//            // if organization priority_pickup is true then available
+//           echo 'This pickup code is available.';
+//           wp_die();
+//         }else {
+//           // if organization priority_pickup is false then not available
+//           echo 'Invalid pickup code';
+//           wp_die();
+//         }
+//   }
+//   wp_die();
+// }
+// add_action('wp_ajax_check_pickup_code', __NAMESPACE__ . '\\is_pickup_code_available_callback');
+// add_action('wp_ajax_nopriv_check_pickup_code', __NAMESPACE__ . '\\is_pickup_code_available_callback');
+// 
 function is_pickup_code_available_callback() {
-  $code = $_POST['code'];
-  $term = get_term_by('name', $code, 'pickup_code');
-  $current_user = wp_get_current_user();
-  $organization_id = get_user_meta( $current_user->ID, 'organization', true );
-  $pickup_settings = get_field('pickup_settings', $organization_id);
-  $priority_pickup = $pickup_settings['priority_pickup'];
-  $assigned_posts = get_posts(['post_type' => 'trans_dept','tax_query' => [['taxonomy' => 'pickup_code','field' => 'term_id','terms' => $term->term_id]]
-  ]);
-  // error_log(print_r($priority_pickup), true);
-  if (empty($assigned_posts)) {
-        //check if its assign to any pickup code if not then available
-    echo 'This pickup code is available.';
-  } else {
-        //if not available validate if the priority_pickup is set to true or false 
-        if($priority_pickup) {
-           // if organization priority_pickup is true then available
-          echo 'This pickup code is available.';
-          wp_die();
-        }else {
-          // if organization priority_pickup is false then not available
-          echo 'Invalid pickup code';
-          wp_die();
-        }
+  $codes = explode(',', $_POST['codes']);
+  $available_codes = array();
+
+  foreach ($codes as $code) {
+    $term = get_term_by('name', trim($code), 'pickup_code');
+    $current_user = wp_get_current_user();
+    $organization_id = get_user_meta($current_user->ID, 'organization', true);
+    $pickup_settings = get_field('pickup_settings', $organization_id);
+    $priority_pickup = $pickup_settings['priority_pickup'];
+    $assigned_posts = get_posts([
+      'post_type' => 'trans_dept',
+      'tax_query' => [
+        [
+          'taxonomy' => 'pickup_code',
+          'field' => 'term_id',
+          'terms' => $term->term_id
+        ]
+      ]
+    ]);
+
+    if (empty($assigned_posts)) {
+      // Check if it's assigned to any pickup code, if not, then it's available
+      $available_codes[] = $code;
+    } else {
+      // If not available, validate if the priority_pickup is set to true or false
+      if ($priority_pickup) {
+        
+        // If organization priority_pickup is true, then it's available
+        $available_codes[] = $code;
+      }
+    }
   }
+
+  if (!empty($available_codes)) {
+    echo 'The following pickup codes are available: ' . implode(', ', $available_codes);
+  } else {
+    echo implode(', ', $codes);
+  }
+
   wp_die();
 }
+
 add_action('wp_ajax_check_pickup_code', __NAMESPACE__ . '\\is_pickup_code_available_callback');
 add_action('wp_ajax_nopriv_check_pickup_code', __NAMESPACE__ . '\\is_pickup_code_available_callback');
+
 
 
 function pickup_form_shortcode() {
@@ -87,8 +134,8 @@ function pickup_form_shortcode() {
 ?>
   <form id="pickup-form">
     <label for="pickup-code">Pickup Codes</label>
-    <p>Enter your five digit pickup codes (i.e.zip codes) to check their availability within our system:</p>
-    <input type="text" id="pickup-code" maxlength = "5"  name="pickup-code" />
+    <p>Enter your five digit pickup codes (i.e.zip codes) to check their availability within our system or separated with comma example 11154,88785,87835.</p>
+    <input type="text" id="pickup-code"  name="pickup-code" />
     <button type="submit" class = "pickupcode-btn">Check Availability</button>
   </form>
       <p><span id="invalid-pickup-count"></p>
@@ -103,10 +150,12 @@ add_shortcode('pickup_form', __NAMESPACE__ . '\\pickup_form_shortcode');
 function add_pickup_code() {
     $pickupcode = $_POST['pickupcode'];
     $term_exists = term_exists($pickupcode, 'pickup_code');
-    error_log(print_r($term_exists));
+  
     $current_user_id = get_current_user_id();
     $organization_id = get_user_meta( $current_user_id, 'organization', true );
      if( $organization_id ){ 
+
+        error_log(print_r('exist choi'));
           $trans_depts = get_org_transdepts( $organization_id );
           $args = [ 'author' => $current_user_id,
                     'post_type' => 'trans_dept',
@@ -130,6 +179,7 @@ function add_pickup_code() {
           // Assign the term to the current post's 'pickup_code' taxonomy
           $term_taxonomy_id = wp_set_object_terms($trans_depts[0], $pickupcode, 'pickup_code', true);
           if (is_wp_error($term_taxonomy_id)) {
+            error_log(print_r('wala ma assign'));
               echo 'Error assigning Pickup Code ' . $pickupcode . ' to current transport department: ' . $term_taxonomy_id->get_error_message();
               wp_die();
           }
