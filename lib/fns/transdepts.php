5258b2037c2879c99ca521dd7ff97e89fec98caa
Michael Wender <michael@michaelwender.com>
2023-07-10 13:03:20 -0400

    `get_trans_dept_contact()` to check parent org for missing Trans Dept contact info

--- lib/fns/transdepts.php
+++ lib/fns/transdepts.php
@@ -169,6 +169,22 @@ function get_trans_dept_contact( $trans_dept_id = '' ) {
       'phone'         => get_field( 'phone', $trans_dept_id ),
     ];
 
+    // Populate any empty fields with the Parent Org's Default Transportation Contact fields:
+    $org_id = null;
+    $org_default_trans_dept_contact = null;
+    $check_for_empty = [ 'contact_title', 'contact_name', 'contact_email', 'phone' ];
+    foreach( $check_for_empty as $array_key ){
+      if( empty( $trans_dept_contact[ $array_key ] ) ){
+        if( empty( $org_id ) )
+          $org_id = get_field( 'organization', $trans_dept_id );
+
+        if( empty( $org_default_trans_dept_contact ) )
+          $org_default_trans_dept_contact = get_field( 'default_trans_dept_contact', $org_id );
+
+        $trans_dept_contact[ $array_key ] = $org_default_trans_dept_contact[ $array_key ];
+      }
+    }
+
     return $trans_dept_contact;
 }
 
