ecba5aa06bb519b6d5e2dc81ed0330222803f823
Michael Wender <michael@michaelwender.com>
2024-02-27 15:26:17 -0500

    Adding shortcodes for displaying link to Org Page in User Portal

--- lib/fns/shortcode/organization-seo-page.php
+++ lib/fns/shortcode/organization-seo-page.php
@@ -3,6 +3,7 @@
 namespace DonationManager\shortcodes;
 use function DonationManager\utilities\{get_alert,get_boilerplate};
 use function DonationManager\transdepts\{get_trans_dept_ids,get_pickup_codes};
+use function DonationManager\templates\{render_template};
 
 function get_organization_seo_page( $atts ){
   extract( shortcode_atts( array(
@@ -39,3 +40,69 @@ function get_organization_seo_page( $atts ){
   return implode( "\n", $html );
 }
 add_shortcode( 'organization-seo-page', __NAMESPACE__ . '\\get_organization_seo_page' );
+
+/**
+ * Gets the HTML for linking to the user's Org Page within the User Portal
+ *
+ * @param      array  $atts {
+ *   @type  int  $org_id The Organization ID.
+ * }
+ *
+ * @return     string  The link to the user's organizational page.
+ */
+function get_user_portal_org_page_html( $atts ){
+  $args = shortcode_atts([
+    'org_id' => null,
+  ], $atts );
+
+  if( is_null( $args['org_id'] ) )
+    return get_alert(['description' => '$org_id is <code>null</code>.', 'type' => 'danger']);
+
+  if( ! is_numeric( $args['org_id'] ) )
+    return get_alert(['description' => '$org_id must be a number.', 'type' => 'danger']);
+
+  if( 'organization' != get_post_type( $args['org_id'] ) )
+    return get_alert(['description' => 'No Organization found for ID ' . $args['org_id'] . '.', 'type' => 'danger']);
+
+  $org_page_url = get_org_page_url( ['org_id' => $args['org_id'] ] );
+
+  return render_template( 'user-portal-organization-page', ['org_page_url' => $org_page_url]);
+}
+add_shortcode( 'user_portal_org_page', __NAMESPACE__ . '\\get_user_portal_org_page_html' );
+
+/**
+ * Returns the permalink for an Organization CPT.
+ *
+ * @param      array  $atts {
+ *   @type  int  $org_id The Organization ID.
+ * }
+ *
+ * @return     string  The organization page url.
+ */
+function get_org_page_url( $atts ){
+    $args = shortcode_atts([
+      'org_id' => null,
+    ], $atts );
+
+    if( is_null( $args['org_id'] ) || ! is_numeric( $args['org_id'] ) ){
+      $current_user = wp_get_current_user();
+      if( $current_user )
+        $org_id = get_user_meta( $current_user->ID, 'organization', true );
+    } else {
+      $org_id = $args['org_id'];
+    }
+
+    $url = '';
+
+    if( 'organization' != get_post_type( $org_id ) )
+      return $url;
+
+    $show_org_page = get_field( 'org_page', $org_id );
+    if( ! $show_org_page )
+      return $url;
+
+    $url = get_permalink( $org_id );
+
+    return $url;
+}
+add_shortcode( 'get_org_page_url', __NAMESPACE__ . '\\get_org_page_url' );
