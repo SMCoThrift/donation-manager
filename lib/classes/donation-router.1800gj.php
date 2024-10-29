79a617815eb7b3ea2ae466b2abbbea57b7084ccb
Michael Wender <michael@michaelwender.com>
2024-10-29 09:13:32 -0400

    Additional data sanitization

--- lib/classes/donation-router.1800gj.php
+++ lib/classes/donation-router.1800gj.php
@@ -20,29 +20,33 @@ class GotJunkDonationRouter extends DonationRouter{
       if( ! defined( 'GOTJUNK_CLIENT_TOKEN' ) || ! GOTJUNK_CLIENT_TOKEN )
         return new \WP_Error( 'no_gotjunk_client_token', 'No 1-800-GOT-JUNK CLIENT TOKEN. Please define a valid `GOTJUNK_CLIENT_TOKEN` in your .env.' );
 
-        $questions = array();
-        if( array_key_exists( 'screening_questions', $donation ) && is_array( $donation['screening_questions'] ) ){
-            foreach( $donation['screening_questions'] as $screening_question ){
-                $questions[] = '- ' . $screening_question['question'] . ' ' . $screening_question['answer'];
-            }
-        }
-        $screening_questions = '';
-        if( 0 < count( $questions ) )
-            $screening_questions = "\n\n# SCREENING QUESTIONS\n" . implode( "\n", $questions );
-        $type_of_items = "# TYPE OF ITEMS\n" . implode( ', ',  $donation['items'] ) . "\n\n# DESCRIPTION OF ITEMS\n" . $donation['description'] . "\n\n# LOCATION OF ITEMS\n" . $donation['pickuplocation'] . $screening_questions;
+      $questions = array();
+      if( array_key_exists( 'screening_questions', $donation ) && is_array( $donation['screening_questions'] ) ){
+          foreach( $donation['screening_questions'] as $screening_question ){
+              $questions[] = '- ' . $screening_question['question'] . ' ' . $screening_question['answer'];
+          }
+      }
+      $screening_questions = '';
+      if( 0 < count( $questions ) )
+          $screening_questions = "\n\n# SCREENING QUESTIONS\n" . implode( "\n", $questions );
+      $type_of_items = "# TYPE OF ITEMS\n" . implode( ', ',  $donation['items'] ) . "\n\n# DESCRIPTION OF ITEMS\n" . $donation['description'] . "\n\n# LOCATION OF ITEMS\n" . $donation['pickuplocation'] . $screening_questions;
 
-        $pickup_address = '';
-        if( 'Yes' == $donation['different_pickup_address'] ){
-            $pickup_address = "\n\n# PICK UP ADDRESS IS DIFFERENT FROM CUSTOMER ADDRESS:\n" . $donation['pickup_address']['address'] . "\n" . $donation['pickup_address']['city'] . ", " . $donation['pickup_address']['state'] . " " . $donation['pickup_address']['zip'] . "\n";
-        }
-        $pickup_dates = array(
-            '- ' . $donation['pickupdate1'] . ', ' . $donation['pickuptime1'],
-            '- ' . $donation['pickupdate2'] . ', ' . $donation['pickuptime2'],
-            '- ' . $donation['pickupdate3'] . ', ' . $donation['pickuptime3']
-        );
+      $pickup_address = '';
+      if( 'Yes' == $donation['different_pickup_address'] ){
+          $pickup_address = "\n\n# PICK UP ADDRESS IS DIFFERENT FROM CUSTOMER ADDRESS:\n" . $donation['pickup_address']['address'] . "\n" . $donation['pickup_address']['city'] . ", " . $donation['pickup_address']['state'] . " " . $donation['pickup_address']['zip'] . "\n";
+      }
+      $pickup_dates = array(
+          '- ' . $donation['pickupdate1'] . ', ' . $donation['pickuptime1'],
+          '- ' . $donation['pickupdate2'] . ', ' . $donation['pickuptime2'],
+          '- ' . $donation['pickupdate3'] . ', ' . $donation['pickuptime3']
+      );
 
-        // $special_instructions = pick updates and $donation['pickup_address']
-        $special_instructions = $type_of_items . "\n\n# PREFERRED PICK UP DATES\n" . implode( "\n", $pickup_dates ) . $pickup_address;
+      // $special_instructions = pick updates and $donation['pickup_address']
+      $special_instructions = $type_of_items . "\n\n# PREFERRED PICK UP DATES\n" . implode( "\n", $pickup_dates ) . $pickup_address;
+      $sanitized_instructions = json_encode( $special_instructions );
+
+      // Remove leading and trailing quotes that json_encode adds to strings
+      $sanitized_instructions = trim($sanitized_instructions, '"');
 
       $args = array(
         'body' => array(
@@ -55,28 +59,28 @@ class GotJunkDonationRouter extends DonationRouter{
           'state' => $donation['address']['state'],
           'country' => 'United States',
           'zip' => $donation['address']['zip'],
-          'customer_type' => 'Residential',
           'language'  => 'English',
-          'company'   => 'null',
-          'notes'     => $special_instructions,
-          'source'    => 'PUMD - Pickup My Donation',
-      ),
-    );
-        $this->save_api_post( $donation['ID'], $args );
+          'notes'     => $sanitized_instructions,
+          'source'    => 'PickUpMyDonation.com (PUMD)',
+        ),
+        'timeout' => 10,
+      );
 
-        // Don't send if we're debugging:
-        /*
-        if( DONMAN_DEV_ENV ){
-            uber_log('INFO: `DONMAN_DEV_ENV` is ON. 1800GJ pickup request not sent.');
-            return;
-        }
-        /**/
+      if( array_key_exists( 'company', $donation['address'] ) && ! empty( $donation['address']['company'] ) ){
+        $args['body']['company'] = $donation['address']['company'];
+        $args['body']['customer_type'] = 'Commercial';
+      } else {
+        $args['body']['customer_type'] = 'Residential';
+      }
 
-        // Testing EP: https://o2ebrands.workflows.oktapreview.com/api/flo/225e48540ee79c7d2463d88dfdf3ff94/invoke
+      $this->save_api_post( $donation['ID'], $args );
 
-        $response = wp_remote_post( GOTJUNK_API_EP . '?clientToken=' . GOTJUNK_CLIENT_TOKEN, $args );
-        //uber_log('🔔 1-800-GOT-JUNK API Response: ' . print_r( $response, true ) );
-        $this->save_api_response( $donation['ID'], $response );
+      $remote_post_url = GOTJUNK_API_EP . '?clientToken=' . GOTJUNK_CLIENT_TOKEN;
+      $response = wp_remote_post( $remote_post_url, $args );
+      if( DONMAN_DEV_ENV ){
+        uber_log( '🔔 1-800-GOT-JUNK POST:' . "\n\n👉 ENDPOINT:\n{$remote_post_url}\n\n👉 REQUEST: " . print_r( $args, true ) . "\n\n👉 REQUEST JSON: " . json_encode( $args ). "\n\n👉 RESPONSE: " . print_r( $response, true ) );
+      }
+      $this->save_api_response( $donation['ID'], $response );
     }
 }
 ?>
