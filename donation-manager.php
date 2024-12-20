4b1b277440a10d456a63f13f27ae32ba91eaff2d
Michael Wender <michael@michaelwender.com>
2024-12-20 17:31:17 -0500

    Documenting 4.9.5

--- donation-manager.php
+++ donation-manager.php
@@ -7,7 +7,7 @@
  * Author URI:      https://mwender.com
  * Text Domain:     donation-manager
  * Domain Path:     /languages
- * Version:         4.9.4
+ * Version:         4.9.5
  *
  * @package         DonationManager
  */
