bcd59035573461258a8a63a0c47888fe7265a421
Michael Wender <michael@michaelwender.com>
2025-02-10 16:12:37 -0500

    Adding documentation

--- lib/fns/shortcode/getalert.php
+++ lib/fns/shortcode/getalert.php
@@ -4,6 +4,22 @@ namespace DonationManager\shortcodes;
 use function DonationManager\templates\{render_template};
 use function DonationManager\utilities\{get_alert};
 
+/**
+ * Shortcode handler for displaying an alert box.
+ *
+ * This shortcode renders an alert box with a specified type, title, 
+ * and optional CSS classes. The alert content is passed as the enclosed content.
+ *
+ * @param array  $atts    {
+ *     Optional. An array of shortcode attributes.
+ *
+ *     @type string $type        The type of alert (e.g., 'info', 'warning', 'error'). Default 'info'.
+ *     @type string $title       The title of the alert. Default null.
+ *     @type string $css_classes Additional CSS classes to apply. Default null.
+ * }
+ * @param string $content The enclosed content to be used as the alert description.
+ * @return string The generated alert box HTML.
+ */
 function get_alert_shortcode( $atts, $content = '' ){
   $args = shortcode_atts([
     'type'        => 'info',
